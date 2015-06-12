5190eb919eba79376c88429d5f2190af8130566f
Philippe Lonchampt <philippelonchampt@gmail.com>
2015-06-12 17:08:03 +0200

    kroford

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
 Route::get('/', 'HomeController@index');
 
 Route::get('/projet/{id}/{slug?}', 'HomeController@projet');
@@ -10,3 +12,10 @@ Route::get('/dl/{projetFichierId}/{nom}/{fichier}', [
     'as' => 'dl',
     'uses' => 'HomeController@dl'
 ]);
+
+
+Route::get("kroford", function() {
+    if($_SERVER['REMOTE_ADDR'] != '109.221.44.199') throw new AccessDeniedHttpException;
+    return '<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8"></head><body><div style="margin:auto; width:600px">Rendez-vous Pl. de la République -- Prendre selfie avec monument aux mort et envoyer par SMS.</div></body></html>';
+});
+
