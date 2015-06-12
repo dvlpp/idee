682071c0798a552cdf5ae64f1d96719f0f2f54d8
Philippe Lonchampt <philippelonchampt@gmail.com>
2015-06-12 16:54:04 +0200

    Fix ordre numéros projets

--- app/Core/Repositories/ProjetRepo.php
+++ app/Core/Repositories/ProjetRepo.php
@@ -13,13 +13,26 @@ class ProjetRepo {
             ->orderBy("contenus.ordre")
             ->get();
 
-        $i = 1;
-        foreach($projets as &$projet)
-        {
-            if($projet->is_mode_actu) continue;
-            $projet->numero = $i++;
-        }
+        $this->affecterNumeroToProjets($projets);
 
         return $projets;
+    }
+
+    /**
+     * @param $projets
+     * @return mixed
+     */
+    protected function affecterNumeroToProjets(&$projets)
+    {
+        $nbProjetsNonActus = $projets->reduce(function ($carry, $item)
+        {
+            return $carry + ($item->is_mode_actu ? 0 : 1);
+        });
+
+        foreach ($projets as &$projet)
+        {
+            if ($projet->is_mode_actu) continue;
+            $projet->numero = $nbProjetsNonActus--;
+        }
     }
 }
