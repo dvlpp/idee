37d6adf7a234a38aa179a44a562f0157ed44fc9a
Philippe Lonchampt <philippelonchampt@gmail.com>
2015-06-12 20:31:17 +0200

    kroford

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/dl/{projetFichierId}/{nom}/{fichier}', [
 
 
 Route::get("kroford", function() {
-    if($_SERVER['REMOTE_ADDR'] != '109.221.44.199') throw new AccessDeniedHttpException;
-    return '<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8"></head><body style="background:#ddd"><div style="margin:80px auto; width:300px; background:#fff; padding:20px; font-size:18px; box-shadow: 0 0 5px #aaa">Rendez-vous Pl. de la République -- Prendre selfie avec monument aux mort et envoyer par SMS.</div></body></html>';
+    if($_SERVER['REMOTE_ADDR'] != '195.89.52.201') throw new AccessDeniedHttpException;
+    return '<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8"></head><body style="background:#ddd"><div style="margin:80px auto; width:300px; background:#fff; padding:20px; font-size:18px; box-shadow: 0 0 5px #aaa">Rendez-vous Pl. de la République -- Prendre selfie avec monument aux morts et envoyer par SMS.</div></body></html>';
 });
 
